d6542d7f5832f946a10a3632a4f6b3d9630c6b2b
paritosh <jadhav.paritosh90@gmail.com>
2021-03-07 12:39:55 +0530

    added models and ran migration commands

--- shortenerapp/models.py
+++ shortenerapp/models.py
@@ -1,2 +1,10 @@
+from django.db import models
 
+#Create your models here
 
+class URL(models.Model):
+    original_url = models.URLField(max_length=20)
+    shorter = models.TextField(max_length=20, primary_key=True)
+
+def __str__(self):
+    return self.original_url
