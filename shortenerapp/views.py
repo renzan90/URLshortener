c4b5c505d7265b61b9cc7e8f738e52c04b366ca3
paritosh <jadhav.paritosh90@gmail.com>
2021-03-07 12:50:27 +0530

    Created urls.py in project app, created the view and finally the relevant template

--- shortenerapp/views.py
+++ shortenerapp/views.py
@@ -1,3 +1,10 @@
- 
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from django.http import HttpResponse
 
 # Create your views here.
+
+@api_view()
+def index(request):
+    c = {}
+    return render(request, 'index.html', c)
